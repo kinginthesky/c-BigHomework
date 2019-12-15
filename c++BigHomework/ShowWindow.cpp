eb0e0a3ad1d286ebfc56526c79392201892256fb
jinshhong <707102127@qq.com>
2019-12-15 16:25:25 +0800

    test

--- c++BigHomework/ShowWindow.cpp
+++ c++BigHomework/ShowWindow.cpp
@@ -32,6 +32,7 @@ void CShowWindow::DoDataExchange(CDataExchange* pDX)
 BEGIN_MESSAGE_MAP(CShowWindow, CDialogEx)
 	ON_BN_CLICKED(IDC_BTN_ADD, &CShowWindow::OnBnClickedBtnAdd)
 	ON_BN_CLICKED(IDC_BTN_MODIFY, &CShowWindow::OnBnClickedBtnModify)
+	ON_BN_CLICKED(IDC_BTN_SEARCH, &CShowWindow::OnBnClickedBtnSearch)
 END_MESSAGE_MAP()
 
 
@@ -55,5 +56,45 @@ void CShowWindow::OnBnClickedBtnModify()
 
 	modifyDlg->Create(IDD_MODIFY_DIALOG);
 	modifyDlg->ShowWindow(SW_SHOW);
+
+}
+
+
+char* G2U(const char* gb2312)
+{
+	int len = MultiByteToWideChar(CP_ACP, 0, gb2312, -1, NULL, 0);
+	wchar_t* wstr = new wchar_t[len + 1];
+	memset(wstr, 0, len + 1);
+	MultiByteToWideChar(CP_ACP, 0, gb2312, -1, wstr, len);
+	len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
+	char* str = new char[len + 1];
+	memset(str, 0, len + 1);
+	WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str, len, NULL, NULL);
+	if (wstr) delete[] wstr;
+	return str;
+}
+
+
+
+
+void CShowWindow::OnBnClickedBtnSearch()
+{
+	// TODO:  在此添加控件通知处理程序代码
+	sqlite3* conn = NULL;
+	//创建或打开数据库
+	int result = sqlite3_open(G2U("D:\\phone.db"), &conn); //如果路径不含中文，可以不用转码，保险起见，建议全部转码
+	if (result != SQLITE_OK) {
+		sqlite3_close(conn);
+		
+		MessageBox(TEXT("创建或者打开数据库失败"));
+	}
+
+
+	std::string SQLString = "CREATE TABLE   test (name CHAR(124),phone CHAR(124),time CHAR(124), status CHAR(4))";
+	BOOL createTableResult = sqlOperation(const_cast<char *>(SQLString.c_str()), &conn);
+	//创建失败
+	if (createTableResult == FALSE)
+		MessageBox(TEXT("创建表失败"));
+
 
 }
